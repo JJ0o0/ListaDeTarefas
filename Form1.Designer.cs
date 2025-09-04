7fde26428c56ca2b3b971b631d4c930ba806ca8e
tarde.cetafaju <tarde.cetafaju@PC03LAB2531>
2025-09-04 14:51:09 -0300

    Adicionado arquivos e feito o projeto.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            addButton = new Button();
+            tarefasGroup = new GroupBox();
+            panelTarefas = new FlowLayoutPanel();
+            removeButton = new Button();
+            tarefasInput = new ComboBox();
+            tarefasGroup.SuspendLayout();
             SuspendLayout();
+            // 
+            // addButton
+            // 
+            addButton.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            addButton.Location = new Point(398, 415);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(32, 23);
+            addButton.TabIndex = 1;
+            addButton.Text = "+";
+            addButton.UseVisualStyleBackColor = true;
+            addButton.Click += addButton_Click;
+            // 
+            // tarefasGroup
+            // 
+            tarefasGroup.Controls.Add(panelTarefas);
+            tarefasGroup.Location = new Point(12, 12);
+            tarefasGroup.Name = "tarefasGroup";
+            tarefasGroup.Size = new Size(418, 397);
+            tarefasGroup.TabIndex = 2;
+            tarefasGroup.TabStop = false;
+            tarefasGroup.Text = "Tarefas";
+            // 
+            // panelTarefas
+            // 
+            panelTarefas.AutoScroll = true;
+            panelTarefas.Location = new Point(6, 22);
+            panelTarefas.Name = "panelTarefas";
+            panelTarefas.Size = new Size(406, 369);
+            panelTarefas.TabIndex = 0;
+            // 
+            // removeButton
+            // 
+            removeButton.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            removeButton.Location = new Point(360, 415);
+            removeButton.Name = "removeButton";
+            removeButton.Size = new Size(32, 23);
+            removeButton.TabIndex = 3;
+            removeButton.Text = "-";
+            removeButton.UseVisualStyleBackColor = true;
+            removeButton.Click += removeButton_Click;
+            // 
+            // tarefasInput
+            // 
+            tarefasInput.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            tarefasInput.FormattingEnabled = true;
+            tarefasInput.Location = new Point(12, 415);
+            tarefasInput.Name = "tarefasInput";
+            tarefasInput.Size = new Size(342, 24);
+            tarefasInput.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(442, 450);
+            Controls.Add(tarefasInput);
+            Controls.Add(removeButton);
+            Controls.Add(tarefasGroup);
+            Controls.Add(addButton);
             Name = "Form1";
             Text = "Lista de Tarefas";
+            tarefasGroup.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button addButton;
+        private GroupBox tarefasGroup;
+        private Button removeButton;
+        private FlowLayoutPanel panelTarefas;
+        private ComboBox tarefasInput;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,134 @@ namespace ListaDeTarefas
 {
     public partial class Form1 : Form
     {
+        public class Tarefa
+        {
+            public CheckBox checkTarefa;
+
+            public Panel container;
+
+            public Tarefa()
+            {
+                checkTarefa = new CheckBox();
+                container = new Panel();
+
+                checkTarefa.AutoSize = true;
+                checkTarefa.Location = new Point(0, 5);
+                checkTarefa.Font = new Font("Arial", 10);
+
+                container.Size = new Size(300, 30);
+                container.Controls.Add(checkTarefa);
+            }
+
+            public void SetText(string text)
+            {
+                checkTarefa.Text = text;
+            }
+
+            public void ConnectEvent(EventHandler evento)
+            {
+                checkTarefa.CheckedChanged += evento;
+            }
+        }
+
+        public List<Tarefa> tarefas = new List<Tarefa>();
+        int spaceY = 10;
+
+        public void AdicionarTarefa(Tarefa tarefa)
+        {
+            tarefa.container.Location = new Point(10, spaceY);
+            panelTarefas.Controls.Add(tarefa.container);
+            spaceY += tarefa.container.Height + 5;
+
+            tarefas.Add(tarefa);
+            tarefasInput.Items.Add(tarefa.checkTarefa.Text);
+        }
+
+        public void RemoverTarefa(string nomeTarefa)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.checkTarefa.Text.Equals(nomeTarefa, StringComparison.OrdinalIgnoreCase));
+
+            if (tarefa != null)
+            {
+                panelTarefas.Controls.Remove(tarefa.container);
+                tarefas.Remove(tarefa);
+                tarefasInput.Items.Remove(tarefa.checkTarefa.Text);
+
+                ReogarnizarTarefas();
+
+                MessageBox.Show("Tarefa removida com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Tarefa não foi achada!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ReogarnizarTarefas()
+        {
+            int y = 10;
+
+            foreach (var tarefa in tarefas)
+            {
+                tarefa.container.Location = new Point(10, y);
+                y += tarefa.container.Height + 5;
+            }
+
+            spaceY = y;
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string nome = tarefasInput.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                return;
+            }
+
+            bool tarefa_existe = tarefas.Any(t => t.checkTarefa.Text.Equals(nome, StringComparison.OrdinalIgnoreCase));
+
+            if (tarefa_existe)
+            {
+                MessageBox.Show("Já existe uma tarefa com esse nome!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            Tarefa nova_tarefa = new Tarefa();
+            nova_tarefa.SetText(tarefasInput.Text);
+
+            nova_tarefa.ConnectEvent((s, ev) =>
+            {
+                if (nova_tarefa.checkTarefa.Checked)
+                {
+                    nova_tarefa.checkTarefa.Font = new Font(nova_tarefa.checkTarefa.Font, FontStyle.Strikeout);
+                }
+                else
+                {
+                    nova_tarefa.checkTarefa.Font = new Font(nova_tarefa.checkTarefa.Font, FontStyle.Regular);
+                }
+
+            });
+
+            AdicionarTarefa(nova_tarefa);
+            tarefasInput.Text = "";
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tarefasInput.Text))
+            {
+                return;
+            }
+
+            RemoverTarefa(tarefasInput.Text.Trim());
+            tarefasInput.Text = "";
         }
     }
 }
